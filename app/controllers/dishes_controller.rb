17297df1e1bdf64359b9e0e8d1f3a71d8d76969c
Raimund Schramm <raimund.schramm@gmx.net>
2016-02-14 18:34:50 +0100

    adds actions new and create and updates index

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,5 +1,29 @@
 class DishesController < ApplicationController
 
   def index
+    @dishes = Dish.all
   end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def create
+    @dish = Dish.new(dish_params)
+
+    respond_to do |format|
+      if @dish.save
+        format.html { redirect_to dishes_path }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def dish_params
+      params.require(:dish).permit(:name)
+    end
+
 end
--- test/controllers/dishes_controller_test.rb
+++ test/controllers/dishes_controller_test.rb
@@ -5,5 +5,20 @@ class DishesControllerTest < ActionController::TestCase
   test 'should get index' do
     get :index
     assert_response :success
+    assert_not_nil assigns(:dishes)
   end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+
+  test "should create dish" do
+    assert_difference('Dish.count') do
+      post :create, dish: { name: 'Currywurst' }
+    end
+
+    assert_redirected_to dishes_path
+  end
+
 end
