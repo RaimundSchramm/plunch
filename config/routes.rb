b1f1645d4e737eacffc19bbca9ee573ecc238592
Raimund Schramm <raimund.schramm@gmx.net>
2016-02-18 18:59:42 +0100

    adds route for meals#new action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   resources :dishes, only: [:index, :new, :create]
-  resources :meals, only: [:index]
+  resources :meals, only: [:index, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
