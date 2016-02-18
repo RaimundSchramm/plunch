42b078d08fd09af90445cede60d835810634d2f5
Raimund Schramm <raimund.schramm@gmx.net>
2016-02-18 18:59:18 +0100

    adds test for meals#new action

--- test/controllers/meals_controller_test.rb
+++ test/controllers/meals_controller_test.rb
@@ -6,4 +6,11 @@ class MealsControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:meals)
   end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:meal)
+  end
+
 end
