7924de390977385393a8d6730126c6f10c0970ee
Raimund Schramm <raimund.schramm@gmx.net>
2016-02-19 11:49:01 +0100

    adds test for meals#create action

--- test/controllers/meals_controller_test.rb
+++ test/controllers/meals_controller_test.rb
@@ -13,4 +13,18 @@ class MealsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:meal)
   end
 
+  test 'should create meal' do
+    assert_difference('Meal.count') do
+      post :create, meal: { eaten_on: Date.today }
+    end
+    assert_redirected_to meals_path
+  end
+
+  test 'should not create meal if invalid' do
+    assert_no_difference('Meal.count') do
+      post :create, meal: { eaten_on: nil }
+    end
+    assert_response :success
+  end
+
 end
