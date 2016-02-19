5c6f1878da541e259e7a7bfb739f77ab224042dc
Raimund Schramm <raimund.schramm@gmx.net>
2016-02-19 11:49:48 +0100

    adds meals#create action to meals controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,4 +6,20 @@ class MealsController < ApplicationController
   def new
     @meal = Meal.new
   end
+
+  def create
+    @meal = Meal.new(meal_params)
+    respond_to do |format|
+      if @meal.save
+        format.html { redirect_to meals_path }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+  def meal_params
+    params.require(:meal).permit(:eaten_on, :dish_id)
+  end
 end
