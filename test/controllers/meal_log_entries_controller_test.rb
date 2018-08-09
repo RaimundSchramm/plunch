e219bc23d2b28e6ac6a79211467ac549256202e9
Raimund Schramm <raimund.schramm@gmx.net>
2018-08-09 15:00:58 +0200

    update controllers for nesting between MealLogs and MealLogEntries:
    - update MealLogEntriesController
    - update test

--- test/controllers/meal_log_entries_controller_test.rb
+++ test/controllers/meal_log_entries_controller_test.rb
@@ -2,48 +2,69 @@ require 'test_helper'
 
 class MealLogEntriesControllerTest < ActionController::TestCase
   setup do
-    @meal_log_entry = meal_log_entries(:one)
+    # @meal_log       = meal_logs(:one)
+    # @meal_log_entry = meal_log_entries(:one)
+
+    MealLog.delete_all
+    MealLogEntry.delete_all
+    @meal_log = MealLog.create
+    @meal_log_entry = @meal_log.meal_log_entries.create(eaten_on: DateTime.new(2018,07,25,8))
   end
 
   test "should get index" do
-    get :index
+    get :index, meal_log_id: @meal_log
     assert_response :success
+    assert_not_nil assigns(:meal_log)
     assert_not_nil assigns(:meal_log_entries)
+    assert_template 'meal_log_entries/index'
   end
 
   test "should get new" do
-    get :new
+    get :new, meal_log_id: @meal_log
     assert_response :success
+    assert_not_nil assigns(:meal_log)
+    assert_not_nil assigns(:meal_log_entry)
+    assert_template 'meal_log_entries/new'
   end
 
   test "should create meal_log_entry" do
     assert_difference('MealLogEntry.count') do
-      post :create, meal_log_entry: { eaten_on: @meal_log_entry.eaten_on, meal: @meal_log_entry.meal }
+      post :create, { meal_log_id: @meal_log, meal_log_entry: { eaten_on: @meal_log_entry.eaten_on, meal: @meal_log_entry.meal } }
     end
 
-    assert_redirected_to meal_log_entry_path(assigns(:meal_log_entry))
+    assert_not_nil assigns(:meal_log)
+    assert_not_nil assigns(:meal_log_entry)
+    assert_redirected_to meal_log_meal_log_entry_path(assigns(:meal_log), assigns(:meal_log_entry))
   end
 
   test "should show meal_log_entry" do
-    get :show, id: @meal_log_entry
+    get :show, meal_log_id: @meal_log, id: @meal_log_entry
     assert_response :success
+    assert_not_nil assigns(:meal_log)
+    assert_not_nil assigns(:meal_log_entry)
+    assert_template 'meal_log_entries/show'
   end
 
   test "should get edit" do
-    get :edit, id: @meal_log_entry
+    get :edit, meal_log_id: @meal_log, id: @meal_log_entry
     assert_response :success
+    assert_not_nil assigns(:meal_log)
+    assert_not_nil assigns(:meal_log_entry)
+    assert_template 'meal_log_entries/edit'
   end
 
   test "should update meal_log_entry" do
-    patch :update, id: @meal_log_entry, meal_log_entry: { eaten_on: @meal_log_entry.eaten_on, meal: @meal_log_entry.meal }
-    assert_redirected_to meal_log_entry_path(assigns(:meal_log_entry))
+    patch :update, meal_log_id: @meal_log, id: @meal_log_entry, meal_log_entry: { eaten_on: @meal_log_entry.eaten_on, meal: @meal_log_entry.meal }
+    assert_not_nil assigns(:meal_log)
+    assert_not_nil assigns(:meal_log_entry)
+    assert_redirected_to meal_log_meal_log_entry_path(assigns(:meal_log), assigns(:meal_log_entry))
   end
 
   test "should destroy meal_log_entry" do
     assert_difference('MealLogEntry.count', -1) do
-      delete :destroy, id: @meal_log_entry
+      delete :destroy, meal_log_id: @meal_log, id: @meal_log_entry
     end
 
-    assert_redirected_to meal_log_entries_path
+    assert_redirected_to meal_log_meal_log_entries_path(assigns(:meal_log))
   end
 end
